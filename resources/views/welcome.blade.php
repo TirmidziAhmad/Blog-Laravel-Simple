3fbe687d288d1a5e6d881d6ec4cd7c528bf84937
tirmidziahmad <tirmidziahmad05@gmail.com>
2024-06-04 02:11:23 +0700

    connecting bootstrap

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style type="text/css">
+        .bt-icons i {
+            font-size: 30px;
+        }
+    </style>
+</head>
+<body>
+    <main class="container">
+        <div class="flex justify-center">
+            <h1>Install Bootstrap in Laravel with CoderAdvise.com</h1>
+        </div>
+        <div class="bt-icons">
+            <i class="bi bi-airplane-fill"></i>
+            <i class="bi bi-apple"></i>
+            <i class="bi bi-balloon-heart"></i>
+        </div>
+    </main>
+</body>
+</html>
